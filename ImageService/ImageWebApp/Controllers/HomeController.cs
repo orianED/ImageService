2399c59ff895ecf2a68cacb78e13912bd4231549
orianED <34006208+orianED@users.noreply.github.com>
2018-06-07 16:31:41 +0300

    k

---
+++ ImageService/ImageWebApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageWebApp.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Config() {
+            ViewBag.Message = "Service Config.";
+
+            return View();
+        }
+
+        public ActionResult Photos() {
+            ViewBag.Message = "Service Photos.";
+
+            return View();
+        }
+
+        public ActionResult Logs() {
+            ViewBag.Message = "Service Logs.";
+
+            return View();
+        }
+    }
+}
