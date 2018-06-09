1ae4d6d205ba02a65fa0d6858593c1d28ce92a9b
Eliyahu Many <alihom3@gmail.com>
2018-06-09 17:59:36 +0300

    Photos model and cshtml

--- ImageService/ImageWebApp/Models/ConfigModel.cs
+++ ImageService/ImageWebApp/Models/ConfigModel.cs
@@ -10,19 +10,28 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Web;
 
 namespace ImageWebApp.Models {
     public class ConfigModel {
         private IClient m_client;
+        private bool request_finished;
         private ObservableCollection<string> m_handlers;
 
         public ConfigModel() {
+            request_finished = false;
+            SourceName = "";
+            LogName = "";
+            ThumbnailSize = "";
+            OutputDir = "";
+            Handlers = new ObservableCollection<string>();
             try {
                 m_client = Client.GetInstance;
                 m_client.DataRecieved += MessageRecieved;
                 m_handlers = new ObservableCollection<string>();
                 m_client.Send((new CommandRecievedEventArgs((int)CommandEnum.GetConfigCommand, null, null)).ToJson());
+                SpinWait.SpinUntil(() => request_finished);
             } catch (Exception e) {
                 Debug.WriteLine("Connection Failure");
             }
@@ -60,9 +69,14 @@ namespace ImageWebApp.Models {
                 ThumbnailSize = (string)json["ThumbnailSize"];
                 string[] arr = ((string)json["Handler"]).Split(';');
                 for (int i = 0; i < arr.Length; i++) {
-                    m_handlers.Add(arr[i]);
+                    if (arr[i] != null && arr[i] != "")
+                        m_handlers.Add(arr[i]);
                 }
                 Handlers = m_handlers;
+                request_finished = true;
+            } else if ((int)CommandEnum.CloseCommand == Int32.Parse((string)json["CommandID"])) {
+                CommandRecievedEventArgs cArgs = CommandRecievedEventArgs.FromJason(e.Message);
+                request_finished = true;
             }
         }
 
@@ -71,10 +85,12 @@ namespace ImageWebApp.Models {
         /// </summary>
         /// <param name="path"></param>
         public void OnRemove(string path) {
+            request_finished = false;
             string[] args = new String[1];
             Handlers.Remove(path);
             args[0] = path;
             m_client.Send(new CommandRecievedEventArgs((int)CommandEnum.CloseCommand, args, null).ToJson());
+            SpinWait.SpinUntil(() => request_finished);
         }
     }
 }
