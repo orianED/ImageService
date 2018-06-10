7b543fa0d9af0e6472ce984cbc06a129a9e61b1a
orianED <34006208+orianED@users.noreply.github.com>
2018-06-10 15:46:02 +0300

    finish merge

--- ImageService/ImageWebApp/Models/PhotosModel.cs
+++ ImageService/ImageWebApp/Models/PhotosModel.cs
@@ -9,11 +9,17 @@ using System.IO;
 using System.Linq;
 using System.Web;
 =======
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 >>>>>>> didi
 
 namespace ImageWebApp.Models {
     public class PhotosModel {
+        private static Regex r = new Regex(":");
         public string OutputDir { get; set; }
         public string ThumbnailsDir { get; set; }
         public string[] Paths { get; set; }
@@ -31,8 +37,32 @@ namespace ImageWebApp.Models {
                 Pictures.Add(new Dictionary<string, string> {
                     {"thumb",str.Replace(OutputDir, "Images") }, {"picture", tempStr}
 =======
+            try {
+                OutputDir = Output;
+                ThumbnailsDir = Path.Combine(Output, "Thumbnails");
+                Paths = Directory.GetFiles(ThumbnailsDir, "*", SearchOption.AllDirectories);
+                string tempStr;
+                foreach (string str in Paths) {
+                    tempStr = str.Replace(ThumbnailsDir, "Images");
+                    Pictures.Add(new Dictionary<string, string> {
+                    {"thumb",str.Replace(OutputDir, "Images") }, {"picture", tempStr},
+                        { "name",  Path.GetFileNameWithoutExtension(tempStr)}, {"date", DateFromPath(tempStr) }
 >>>>>>> didi
                 });
             }
         }
+
+        public string DateFromPath(string path) {
+            try {
+                using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                using (Image myImage = Image.FromStream(fs, false, false)) {
+                    PropertyItem propItem = myImage.GetPropertyItem(36867);
+                    string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    return DateTime.Parse(dateTaken).ToString();
+                }
+            } catch (Exception e) {
+                return File.GetCreationTime(path).ToString();
+            }
+        }
     }
+}
